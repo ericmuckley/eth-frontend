359cbf3deaa2750598512a1289b271b2d2cbfdc6
Eric Muckley <ericmuckley@gmail.com>
2021-11-29 21:18:36 -0800

    Add files via upload

--- static/js/init-app.js
+++ static/js/init-app.js
@@ -1,87 +1,161 @@
 
 
-var cgPriceUrl = "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false";
-
-var openseaAssetsUrl = "https://api.opensea.io/api/v1/assets?owner=0x91a42eCF59abB8350A0A64c8D3f5eb3bD08cc6c2";
-
-
-/*
-
-function loadXMLDoc(url) {
-    var xmlhttp = new XMLHttpRequest();
-
-
-    xmlhttp.onreadystatechange = function() {
-        if (xmlhttp.readyState == XMLHttpRequest.DONE) {
-           if (xmlhttp.status == 200) {
-               document.getElementById("price-results").innerHTML = xmlhttp.responseText;
-           } else {
-               alert('something else other than 200 was returned');
-           }
-        }
-    };
-
-    xmlhttp.open("GET", url, true);
-    xmlhttp.send();
-};
-*/
-
-//loadXMLDoc(cgPriceUrl);
+const ETHERSCAN_API_KEY = "9FBNJFFAW5H9X27RTUNVDTG3KH1X3VQVCI";
 
 
 
 
 
 
-// Using fetch method for an HTTP request
-// https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API/Using_Fetch
 
-async function httpRequest(url, method="GET", data={}) {
-    // Default options are marked with *
-    const response = await fetch(url, {
-        method: method, // *GET, POST, PUT, DELETE, etc.
-        mode: 'cors', // no-cors, *cors, same-origin
-        cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
-        credentials: 'same-origin', // include, *same-origin, omit
-        headers: {
-            'Content-Type': 'application/json'
-            // 'Content-Type': 'application/x-www-form-urlencoded',
-        },
-        redirect: 'follow', // manual, *follow, error
-        referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
-        //...(method === "POST" && {body: JSON.stringify(data)}),
-        //body: JSON.stringify(data), // body data type must match "Content-Type" header
+// get address information
+document.getElementById("submit-address").onclick = function () {
+    document.getElementById("assets-loading").style.display = "block";
+    document.getElementById("assets-content").innerHTML = "";
+
+    var myaddress = document.getElementById("address-input").value;
+    var openseaAssetsUrl = "https://api.opensea.io/api/v1/assets?owner=" + myaddress;
+
+
+
+
+    var ETHERSCAN_GET_ETH_URL = `https://api.etherscan.io/api?module=account&action=balance&address=${myaddress}&tag=latest&apikey=${ETHERSCAN_API_KEY}`;
+    httpRequest(ETHERSCAN_GET_ETH_URL)
+    .then(data => {
+        console.log(data['result'] / 1e18);
     });
-    return response.json(); // parses JSON response into native JavaScript objects
+
+
+
+
+
+
+
+
+
+
+    // load the address assets using the opensea API
+    httpRequest(openseaAssetsUrl)
+    .then(data => {
+        console.log(data);
+        //document.getElementById("assets-content").innerHTML = JSON.stringify(data, null, 4);
+
+
+        // get NFT info out of opensea assets
+        var nfts = data["assets"];
+         document.getElementById("assets-content").innerHTML += '<h3>NFTs</h3>';
+
+        var row = createRowCols('assets-content', ncols=nfts.length)
+        row.style.width = "100%";
+        row.style.overflow = "auto";
+        var nft_i = 0;
+
+        for (let nft of nfts) {
+
+            cardObj = createCard(config={"img": true, "header": false, "body": true, "footer": true});
+            //cardObj['card'].style = "width: 18rem";
+            cardObj['img'].src = nft['image_url'];
+            cardObj['img'].alt = nft['name'] + ' image';
+
+            var cardtitle = document.createElement('h3');
+            cardtitle.innerHTML = nft['name'];
+            cardObj['body'].appendChild(cardtitle);
+
+            var p = document.createElement('p');
+            p.classList.add('lead');
+            p.innerHTML = nft['description'] !== null ? nft['description'] : "no description";
+            cardObj['body'].appendChild(p);
+
+            var a = document.createElement('a');
+            a.href = nft['permalink'];
+            a.target = "_blank";
+            a.text = "View on OpenSea";
+            cardObj['footer'].appendChild(a);
+
+            row.children[nft_i].appendChild(cardObj['card']);
+            nft_i += 1;
+
+            //document.getElementById("assets-content").appendChild(cardObj['card']);
+
+        };
+
+
+    });
+
+
+
+
+
+    document.getElementById("assets-loading").style.display = "none";
+
+
 };
 
 
 
-httpRequest(cgPriceUrl)
+
+
+
+
+
+
+
+
+
+// load the price feed information using the CoinGecko API
+const COINGECKO_PRICE_URL = "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false";
+httpRequest(COINGECKO_PRICE_URL)
 .then(data => {
-    //console.log(data); // JSON data parsed by `data.json()` call
-    //document.getElementById("price-results").innerHTML = JSON.stringify(data, null, 4);
-    
-    
-    var keys = Object.keys(data[0]);
-        
+
+    //var keys = Object.keys(data[0]);
+    var keys = ["", "name", "current_price", "market_cap", "price_change_percentage_24h"];
+    var mapping = {
+        "current_price": "Price ($)",
+        "market_cap": "Market cap ($B)",
+        "price_change_percentage_24h": "24 hr change (%)",
+    };
 
     // create the price feed table
     var [table, thead, tbody] = createTable(
-        headers=keys,
-        classes=['table', 'table-sm', 'table-borderless'],
+        headers=formatLabels(keys, mapping=mapping),
+        classes=['table', 'table-sm', 'table-striped'],
         parentdiv='price-feed-div',
         id='price-feed-table',
     );
     table.style.width = "100%";
     table.style.overflow = "auto";
     
+
     // populate price feed table
     for (let coin of data) {
-        var row = thead.insertRow()
+        var row = tbody.insertRow()
         for (let k of keys) {
-            var cell = row.insertCell()
-            cell.innerHTML = coin[k];
+            var cell = row.insertCell();
+            
+            if (k === ""){
+            
+                var img = document.createElement('img');
+                img.src = coin["image"];
+                img.style.height = '30px';
+                img.style.width = '30px';
+                img.style.display = "block";
+                cell.appendChild(img);
+            
+            } else if (k === "market_cap") {
+                cell.innerHTML = coin[k] / 1000000000;
+            } else if (k === "price_change_percentage_24h") {
+                if (coin[k].toString().startsWith("-")) {
+                    cell.innerHTML = coin[k];
+                    cell.style.color = 'red';
+                } else {
+                    cell.innerHTML = "+" + coin[k];
+                    cell.style.color = 'green';
+                };
+    
+            } else {
+            
+                cell.innerHTML = coin[k];
+            };
         };
     };
 
@@ -90,37 +164,10 @@ httpRequest(cgPriceUrl)
         $("#price-feed-table").DataTable({});
     });
     
+    document.getElementById("price-feed-loading").style.display = "none";
+    document.getElementById("price-feed-content").style.display = "block";
+    
 });
-
-
-/*
-httpRequest(openseaAssetsUrl)
-.then(data => {
-    //console.log(data); // JSON data parsed by `data.json()` call
-    document.getElementById("nft-results").innerHTML = JSON.stringify(data, null, 4);
-});
-
-*/
-
-
-
-
-
-
-
-
-
-
-
-
-
-// initialize all hover tooltips in the application
-var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
-var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
-  return new bootstrap.Tooltip(tooltipTriggerEl)
-})
-
-
 
 
 
--- static/js/utils.js
+++ static/js/utils.js
@@ -1,5 +1,5 @@
 // create a table
-function createTable(headers=[], classes=['table', 'table-sm', 'table-borderless'], parentdiv=null, id=null){
+function createTable(headers=[], classes=['table'], parentdiv=null, id=null){
     const table = document.createElement("table");
     var thead = document.createElement('thead');
     var tbody = document.createElement('tbody');
@@ -25,6 +25,31 @@ function createTable(headers=[], classes=['table', 'table-sm', 'table-borderless
 };
 
 
+
+
+function createCard(config={"img": true, "header": true, "body": true, "footer": true}) {
+    var cardObj = {};
+    var card = document.createElement("div");
+    card.classList.add("card");
+    cardObj['card'] = card;
+    if (config['img']) {
+        var img = document.createElement('img');
+        img.classList.add('card-img-top');
+        card.appendChild(img);
+        cardObj['img'] = img;
+    };
+    for (let key of ['header', 'body', 'footer']){
+        if (config[key]) {
+            var div = document.createElement("div");
+            div.classList.add('card-' + key);
+            card.appendChild(div);
+            cardObj[key] = div;
+        };
+    };
+    return cardObj;
+};
+
+
 function createRowCols(parentdiv, ncols=2) {
     // Create a bootstrap row with multiple columns
     const row = document.createElement('div');
@@ -34,7 +59,55 @@ function createRowCols(parentdiv, ncols=2) {
         col.classList.add('col');
         row.appendChild(col);
     };
-    parentdiv.appendChild(row);
+    document.getElementById(parentdiv).appendChild(row);
     return row
 };
 
+
+
+// format a label for display by capitalizing the
+// first letter and replacing underscores with spaces.
+// use the "mapping" argument object to map specific labels
+// to new ones.
+function formatLabels(labels, mapping={}){
+    var new_labels = [];
+    for (let label of labels) {
+        var new_label = null;
+    
+        if (mapping.hasOwnProperty(label)) {
+            new_label = mapping[label];
+        } else {
+	        if (label === '') {
+		        new_label = '';
+	        } else {
+	            new_label = (label.charAt(0).toUpperCase() + label.slice(1));
+	            new_label = new_label.replace('_', ' ');
+	        };
+	    };
+	    new_labels.push(new_label);
+	};
+	return new_labels;
+};
+
+
+
+
+// Using fetch method for an HTTP GET request
+// https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API/Using_Fetch
+async function httpRequest(url, method="GET", data={}) {
+    const response = await fetch(url, {
+        method: method,
+        headers: {'Content-Type': 'application/json'},
+        //...(method === "POST" && {body: JSON.stringify(data)}),
+        //body: JSON.stringify(data), // body data type must match "Content-Type" header
+    });
+    return response.json();
+};
+
+
+
+// initialize all hover tooltips in the application
+var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+  return new bootstrap.Tooltip(tooltipTriggerEl)
+})
