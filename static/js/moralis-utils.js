260dbf1a55b219b951ba9858e387cf79d647985d
ericmuckley <ericmuckley@gmail.com>
2021-12-02 22:29:32 -0800

    changed moralis code

--- static/js/moralis-utils.js
+++ static/js/moralis-utils.js
@@ -1,8 +1,3 @@
-//const INFURA_URI = "https://mainnet.infura.io/v3/9104b3a76b4b46b29c1cc489338980b8";
-//const web3 = new Web3(new Web3.providers.HttpProvider(INFURA_URI));
-//const myAddress = "0x91a42eCF59abB8350A0A64c8D3f5eb3bD08cc6c2";
-
-
 const serverUrl = "https://mysaxj65vywx.usemoralis.com:2053/server";
 const appId = "iPXKdV96rgPUw4gJAxNnJw5gPRBKeGOwX8vFtQHZ";
 Moralis.start({ serverUrl, appId });
---
+++ static/js/web3-utils.js
@@ -0,0 +1,7 @@
+
+const INFURA_URI = "https://mainnet.infura.io/v3/9104b3a76b4b46b29c1cc489338980b8";
+const web3 = new Web3(new Web3.providers.HttpProvider(INFURA_URI));
+
+//const myAddress = "0x91a42eCF59abB8350A0A64c8D3f5eb3bD08cc6c2";
+
+
